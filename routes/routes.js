4bc988a677cbc5aa00ce32b0bb9405ed169223a6
philackm <philackm@icloud.com>
2017-03-18 12:51:40 +1100

    Add database connections

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,49 @@
 var appRouter = function(app) {
-    
+
+    var Sequelize = require('sequelize');
+    var sequelize = new Sequelize('seed-db', 'teamseed@seeddb.database.windows.net', 'seed?1234', {
+    host: 'seeddb.database.windows.net',
+    dialect: 'mssql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    dialectOptions: {
+        encrypt: true
+    }
+    });
+
+
+    var Users = sequelize.define('users', {
+    firstName: {
+        type: Sequelize.STRING,
+        field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+    }, {
+    freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    Users.sync({force: true}).then(function () {
+        // Table created
+        return Users.create({
+            firstName: 'John',
+            lastName: 'Hancock'
+        });
+    });
+
     app.get("/", function(req, res) {
-        res.send("Hello World");
+
+        var greeting = "Hello world."
+
+        Users.findOne().then(function (user) {
+            greeting += user.get('firstName');
+            res.send(greeting);
+        });
+        
     });
 
     app.get("/messages/", function(req, res) {
--- server.js
+++ server.js
@@ -9,43 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js")(app);
  
 
-/*
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('seed_database', 'b1deb7f7316124', 'e639b44a', {
-  host: 'au-cdbr-azure-southeast-a.cloudapp.net',
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  dialectOptions: {
-    encrypt: true
-  }
-});
-*/
 
-/*
-var User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
 
-User.sync({force: true}).then(function () {
-  // Table created
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
-  });
-});
-*/
 
 
 var server = app.listen(process.env.PORT || 1337, function () {
